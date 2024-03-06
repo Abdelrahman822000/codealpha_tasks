1acc1bc8da86bac4f2a08916a601fd487fc0d3f5
Abdelrahman822000 <161854034+Abdelrahman822000@users.noreply.github.com>
2024-03-06 19:41:41 +0200

    Add files via upload

---
+++ Task1_HangmanGame_Final_Version 1.5.5/Hangman_Game.py
@@ -0,0 +1,10 @@
+## Author  : Abdelrahman Mohamed Hamad
+## Date    : 5/3/2024
+## Version : 1.5.5
+
+#!/usr/bin/env python3
+
+from Hangman_Game_Functions import Hangman_Game_GUI
+
+if __name__ == '__main__':
+    Hangman_Game_GUI()
